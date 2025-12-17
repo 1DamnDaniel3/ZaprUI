ff710ec73d7660819b5c32d1bde01700f47c0392
Никита <nikitval@list.ru>
2025-12-17 20:01:02 +0300

    фыв

--- frontend/src/entities/BatCard/ui/BatCard.tsx
+++ frontend/src/entities/BatCard/ui/BatCard.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import s from './BatCard.module.scss';
 
 export function BatCard({ id, path }: { id: number; path: string }) {
+    const [animate, setAnimate] = useState(false)
+
+    const handleClick = () => {
+        setAnimate(true);
+        setTimeout(() => setAnimate(false), 200);
+    }
+
     return (
-        <div className={s.batItem}>
-            {path ?<span className={s.batPath}>{path.split('\\').pop()}</span> : 'Выбери файл'}
+        <div className={`${s.batItem} ${animate ? s.animate : ''}`} onClick={handleClick}>
+            {path ? <span className={s.batPath}>{path.split('\\').pop()}</span> : 'Выбери файл'}
         </div>
     );
 }
