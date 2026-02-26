f5dd99274f64fd4ee0f514e57c3fa6e71ed3714f
Никита <nikitval@list.ru>
2026-02-26 15:42:02 +0300

    Add new leaf images to the shared assets
    - Added leaf2.png, leaf3.png, leaf4.png, leaf5.png, and leaf6.png to the leaves directory.
    - These images enhance the visual assets available for the frontend.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,57 @@
-import './App.css';
-import Snowfall from 'react-snowfall';
-import { RunZapret, WindowControls } from './widgets';
-import { useTheme } from './shared/hooks/useTheme';
-import { useAppInitialization } from './shared/hooks/useAppInitialization';
-import { usePreventZoom } from './shared/hooks/usePreventZoom';
-import { useBat } from './shared/hooks/useBat';
+import "./App.css";
+import Snowfall from "react-snowfall";
+import { RunZapret, WindowControls } from "./widgets";
+import { useTheme } from "./shared/hooks/useTheme";
+import { useAppInitialization } from "./shared/hooks/useAppInitialization";
+import { usePreventZoom } from "./shared/hooks/usePreventZoom";
+import { useBat } from "./shared/hooks/useBat";
+import { leaves } from "./shared/assets/images";
+import { useEffect, useState } from "react";
+
+const leavesSrc = leaves;
 
 function App() {
-    const { theme } = useTheme()
-    const { batRunning } = useBat()
-    const { isInitialized } = useAppInitialization()
-    usePreventZoom()
+    const { theme } = useTheme();
+    const { batRunning } = useBat();
+    const { isInitialized } = useAppInitialization();
+    usePreventZoom();
+
+    const now = new Date();
+    const month = now.getMonth(); // 0 = январь, 11 = декабрь
+    const isWinter: boolean = month === 11 || month === 0 || month === 1;
+    const isSpring: boolean = month >= 2 && month <= 4;
+    const isSummer: boolean = month >= 5 && month <= 7;
+    const isAutumn: boolean = month >= 8 && month <= 10;
+
+    const [leafImages, setLeafImages] = useState<HTMLImageElement[]>([]);
+
+    let images,
+        radius = [0.5, 3.0],
+        snowflakeCount;
+
+    if (isSpring) {
+        radius = [0.5, 3.0];
+        snowflakeCount = 100;
+    } else if (isSummer) {
+        radius = [0.5, 3.0];
+        snowflakeCount = 100;
+    } else if (isAutumn) {
+        images = leafImages;
+        radius = [20, 28];
+        snowflakeCount = 16;
+    } else if (isWinter) {
+        radius = [0.5, 3.0];
+        snowflakeCount = 150;
+    }
+
+    useEffect(() => {
+        const images = leavesSrc.map((src) => {
+            const img = new Image();
+            img.src = src;
+            return img;
+        });
+        setLeafImages(images);
+    }, []);
 
     if (!isInitialized) {
         return null;
@@ -18,11 +59,16 @@ function App() {
 
     return (
         <div id="App">
-            <Snowfall color={theme === 'light' && !batRunning ? '#aaa' : '#ededed'} />
+            <Snowfall
+                color={theme === "light" && !batRunning ? "#aaa" : "#ededed"}
+                images={images}
+                radius={[radius[0], radius[1]]}
+                snowflakeCount={snowflakeCount}
+            />
             <WindowControls />
             <RunZapret />
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
---
+++ frontend/src/shared/assets/images/index.ts
@@ -0,0 +1,7 @@
+import leaf1 from "./leaves/leaf1.png";
+import leaf2 from "./leaves/leaf2.png";
+import leaf3 from "./leaves/leaf3.png";
+import leaf4 from "./leaves/leaf4.png";
+import leaf5 from "./leaves/leaf5.png";
+import leaf6 from "./leaves/leaf6.png";
+export const leaves = [leaf1, leaf2, leaf3, leaf4, leaf5, leaf6];
